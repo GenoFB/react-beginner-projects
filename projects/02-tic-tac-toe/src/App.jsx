7b5e94aabb1ff2f407103deaed99188ee82f7622
geno <genaro.flores.bustos1@gmail.com>
2025-06-04 22:45:26 -0400

    first commit

---
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const board = Array(9).fill(null)
+
+const Square = ({ children, updateBoard, index }) => {
+  return (
+
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
+
+function App() {
+
+  return (
+    <main className='board'>
+      <h1>Tic tac toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+              >
+
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
+  )
+}
+
+export default App
