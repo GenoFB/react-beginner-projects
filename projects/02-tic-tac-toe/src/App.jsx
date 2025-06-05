94d913ebef671d3b583a939d293fbad22cd4c698
geno <genaro.flores.bustos1@gmail.com>
2025-06-05 13:06:11 -0400

    basic functions, showing x and o, but no winner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -6,18 +6,47 @@ const TURNS = {
   O: 'o'
 }
 
-const board = Array(9).fill(null)
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
 
-const Square = ({ children, updateBoard, index }) => {
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-
-    <div className='square'>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
+
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) //null es que no hay ganador y false es que hay un empate
+
+  const updateBoard = (index) => {
+    if (board[index]) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
 
   return (
     <main className='board'>
@@ -29,12 +58,18 @@ function App() {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
-
+                {board[index]}
               </Square>
             )
           })
         }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   )
