8d2e710ab027724b4d8d9683f44f9280b7ec0d34
geno <genaro.flores.bustos1@gmail.com>
2025-07-14 11:37:30 -0400

    enable mouse following working, but no disable yet

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,18 +1,42 @@
 import { useEffect, useState } from 'react'
 function App () {
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
-    console.log('efecto')
-  }, [])
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-    <>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+
       <h3>Proyecto 3: Mouse Follower!</h3>
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
-    </>
+    </main>
   )
 }
 
