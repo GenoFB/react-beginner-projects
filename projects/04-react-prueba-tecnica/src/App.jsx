e2f24a9e4efda471817403c390e88f0296f0490e
geno <genaro.flores.bustos1@gmail.com>
2025-07-17 12:14:09 -0400

    project working but super ugly

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from 'react'
+const CAT_ENDOPOINT_RANDOM_FACT_URL = 'https://catfact.ninja/fact'
+
 export function App () {
+  const [fact, setFact] = useState('Random Fact :)')
+
+  useEffect(() => {
+    fetch(CAT_ENDOPOINT_RANDOM_FACT_URL)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
+
+  function factFirstWord (fact) {
+    return fact.split(' ')[0]
+  }
+  const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${factFirstWord(fact)}`
+
   return (
-    <h1>App de gatitos cute</h1>
+    <main>
+      <h1>App de gatitos cute</h1>
+      {CAT_ENDPOINT_IMAGE_URL && <img src={CAT_ENDPOINT_IMAGE_URL} alt='' />}
+    </main>
   )
 }
