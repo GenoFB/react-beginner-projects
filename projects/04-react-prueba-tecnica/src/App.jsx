88be9f7ce2d684be5fc266d6605781f773cb7004
geno <genaro.flores.bustos1@gmail.com>
2025-07-18 10:26:12 -0400

    Adjusted layout to horizontal alignment

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -27,8 +27,10 @@ export function App () {
   return (
     <main>
       <h1>App de gatitos cute</h1>
-      <p>{fact}</p>
-      {imgUrl && <img src={`${imgUrl}`} alt={`Image extracted using the first three words of the fact: ${fact}`} />}
+      <section>
+        <p>{fact}</p>
+        {imgUrl && <img src={`${imgUrl}`} alt={`Image extracted using the first three words of the fact: ${fact}`} />}
+      </section>
     </main>
   )
 }
