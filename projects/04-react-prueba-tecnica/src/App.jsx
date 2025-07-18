329b4488b23bb270d7c9b3b57511402719efb917
geno <genaro.flores.bustos1@gmail.com>
2025-07-18 10:11:33 -0400

    Refactored the app - now working as expected

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,24 +1,33 @@
 import { useEffect, useState } from 'react'
 const CAT_ENDOPOINT_RANDOM_FACT_URL = 'https://catfact.ninja/fact'
+const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/'
 
 export function App () {
   const [fact, setFact] = useState('Random Fact :)')
+  const [imgUrl, setImgUrl] = useState('')
 
   useEffect(() => {
     fetch(CAT_ENDOPOINT_RANDOM_FACT_URL)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
-  }, [])
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
 
-  function factFirstWord (fact) {
-    return fact.split(' ')[0]
-  }
-  const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${factFirstWord(fact)}`
+        const firstWord = fact.split(' ', 3).join(' ')
+
+        fetch(`${CAT_ENDPOINT_IMAGE_URL}${firstWord}`)
+          .then(data => {
+            const { url } = data
+            setImgUrl(url)
+          })
+      })
+  }, [])
 
   return (
     <main>
       <h1>App de gatitos cute</h1>
-      {CAT_ENDPOINT_IMAGE_URL && <img src={CAT_ENDPOINT_IMAGE_URL} alt='' />}
+      <p>{fact}</p>
+      {imgUrl && <img src={`${imgUrl}`} alt={`Image extracted using the first three words of the fact: ${fact}`} />}
     </main>
   )
 }
