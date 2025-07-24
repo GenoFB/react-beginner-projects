41956060244d79dc60972ee7560ecd272deb15f6
geno <genaro.flores.bustos1@gmail.com>
2025-07-24 11:39:26 -0400

    feat: render movie list using local JSON data extracted from API

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,6 +1,11 @@
+import positiveResults from './mocks/positive-results.json'
+import negativeResults from './mocks/negative-results.json'
 import './App.css'
 
 function App () {
+  const movies = positiveResults.Search
+  const hasMovies = movies?.length > 0
+
   return (
     <div className='page'>
       <header>
@@ -12,7 +17,25 @@ function App () {
       </header>
 
       <main>
-        acá van los resultados
+        {
+          hasMovies
+            ? (
+              <ul className='movies'>
+                {
+                  movies.map(movie => (
+                    <li key={movie.imdbID} className='movie'>
+                      <h3>{movie.Title}</h3>
+                      <p>{movie.Year}</p>
+                      <img src={movie.Poster} alt={movie.Title} />
+                    </li>
+                  ))
+                }
+              </ul>
+              )
+            : (
+              <p>No se encontraron películas para esta búsqueda</p>
+              )
+          }
       </main>
     </div>
   )
